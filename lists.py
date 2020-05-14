abab5187dd40111db53e157e138d6e0ff3f16e0f
Shreekar Uday Pujari <shreekar.pujari@ad.infosys.com>
2020-05-14 17:32:35 +0530

    lists

--- lists.py
+++ lists.py
@@ -1,17 +1,43 @@
 
-friends = ["foo", "bar", "baz", "somefriend"]
-
+lucky_numbers = [4, 8, 15, 16, 23, 42]
+friends = ["Kevin", "Karen", "Jim", "Oscar", "Toby"]
 print(friends)
-print(friends[0])
+# extend allows us to append another list to the end of the first list
+# friends.extend(lucky_numbers)
+# print(friends)
+# append allows us to append to add another item to the list
+# append always adds to the end of the list
+friends.append("Creed")
+print(friends)
+# insert allows us to add item to a list at any desired position
+friends.insert(1, "Kelly")
+print(friends)
+# remove allows us to remove item from a list
+friends.remove("Jim")
+print(friends)
+# pop allows us to remove element from the end
+friends.pop()
+print(friends)
+# clears all the elements
+# friends.clear()
+# print(friends)
 
-#negative indexing in python
+# get the index of an element in list
+print(friends.index("Kelly"))
 
-print(friends[-1])
+friends.insert(2, "Jim")
+friends.insert(3, "Jim")
+# get the count of elements in the list
+print(friends.count("Jim"))
+# sort
+friends.sort()
+print(friends)
+# reverse
+friends.reverse()
+print(friends)
+# copy
+office_friends = friends.copy()
+print(office_friends)
 
-#printing range vals
 
-print(friends[1:3])
 
-friends[0] = "foobar"
-
-print(friends[0])
