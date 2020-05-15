2840c6c44cf70f5cea802a13b8657a8477dad3d1
Shreekar Uday Pujari <shreekar.pujari@ad.infosys.com>
2020-05-15 18:48:32 +0530

    functions and tupples

---
+++ functions.py
@@ -0,0 +1,14 @@
+
+
+def say_hi(name):
+    print("Hello " + name)
+
+
+say_hi("Shreekar")
+
+def cube(num):
+    return pow(num, 3)
+
+
+print(cube(3))
+
---
+++ tupples.py
@@ -0,0 +1,10 @@
+
+#  tuple is an IMMUTABLE sequence of Python objects.
+#  Tuples are sequences, just like lists.
+#  The differences between tuples and lists are, the tuples cannot be changed unlike lists and
+#  tuples use parentheses, whereas lists use square brackets
+
+coordinates = (4, 5)
+print(coordinates[0])
+
+
